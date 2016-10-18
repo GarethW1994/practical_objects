4b652d44937eafb1e45c92e7f616f37f636889af
GarethW1994 <williams.gareth0@gmail.com>
2016-10-18 16:09:46 +0200

    Adding file

--- mostWordsEndsWith.js
+++ mostWordsEndsWith.js
@@ -1,26 +1,62 @@
 var sentence = 'Down by the river there is a man that quiver and shiver, but he needs to deliver a packet that he think is a big racket and a packet of gum.'
 
 var mostWordsEndsWith = function(thisSentence) {
-  var splitSentence = thisSentence.split(' ');
-  //var emptArr = [];
-  var obj = {};
-  //var modalWord = ;
-  //console.log(modalWord);
-  for (var i = 0; i < splitSentence.length; i++) {
+    //define variables (THRIVE ON VARIABLES!!)
+    var splitSentence = thisSentence.split(' ');
+    var letterOccuranceArr = [];
+    var wordsArr = [];
+    var objOutput = [];
 
+    var obj = {};
+
+    var commonWordArr = [];
+    var mostCommonLetter = '';
+
+    //for-loop
+    for (var i = 0; i < splitSentence.length; i++) {
         var currentWord = splitSentence[i];
+        wordsArr.push(currentWord);
+        var EndLetters = currentWord[currentWord.length - 1];
 
-        if (obj[currentWord] === undefined){
-            obj[currentWord] = 0;
+        if (obj[EndLetters] === undefined) {
+            obj[EndLetters] = 0;
         }
-            obj[currentWord]++
-
-            if (currentWord.endsWith('r')) {
-              console.log(currentWord);
-            }
+        obj[EndLetters]++
     }
 
-//console.log(obj);
-//console.log(obj);
+    //for in loop to push items to object
+    for (var x in obj) {
+        letterOccuranceArr.push(obj[x]);
+    }
+
+    //Get the highest value in array with numbers of letter occurences
+    var highVal = Math.max.apply(null, letterOccuranceArr);
+
+    //for in loop through object to get most common key
+    for (var key in obj) {
+        if (obj[key] == highVal) {
+            mostCommonLetter = key;
+        }
+    }
+
+    //add the words ending with the most common letter in array
+    for (var word in wordsArr) {
+        if (wordsArr[word].endsWith(mostCommonLetter)) {
+            commonWordArr.push(wordsArr[word]);
+        }
+    }
+
+    //store final output in var
+    var output = 'The most common letter: ' + mostCommonLetter;
+    var output2 = ' - words: ' + commonWordArr.join(' , ');
+
+    //concat output
+    return output + output2
 }
-mostWordsEndsWith(sentence);
+const assert = require('assert');
+
+var test = mostWordsEndsWith(sentence);
+
+assert.deepEqual(test, 'The most common letter: t - words: that , but , packet , that , racket , packet')
+
+console.log(mostWordsEndsWith(sentence));
